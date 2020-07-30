94e56d0bdfa2a2d8f77c529e28e61850d2f3535d
michaldrabik <michal.drabik0@gmail.com>
2020-07-30 12:10:22 +0200

    Update dependencies

--- app/src/main/java/com/michaldrabik/muzeipixelartextension/App.kt
+++ app/src/main/java/com/michaldrabik/muzeipixelartextension/App.kt
@@ -1,13 +1,9 @@
 package com.michaldrabik.muzeipixelartextension
 
 import android.app.Application
-import com.crashlytics.android.Crashlytics
-import com.crashlytics.android.answers.Answers
-import io.fabric.sdk.android.Fabric
 
 class App : Application() {
   override fun onCreate() {
     super.onCreate()
-    Fabric.with(this, Crashlytics(), Answers())
   }
 }
